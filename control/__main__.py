b926f8eb69b79cd06b9ae859c206c468d87737fd
Allysson Lukas <alla@cesar.org.br>
2019-07-05 02:56:04 -0300

    Add initial main structure
    Add initial main stucture, including glib mainloop, argument parser
    and setuptools.

---
+++ control/__main__.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2019, CESAR. All rights reserved.
+#
+# SPDX-License-Identifier: BSD 3-Clause
+
+import os
+import sys
+import signal
+import lockfile
+import logging
+import argparse
+import dbus
+import dbus.service
+import dbus.mainloop.glib
+import gobject as GObject
+import daemon
+
+mainloop = None
+
+logging.basicConfig(format='[%(levelname)s] %(funcName)s: %(message)s\n',
+                    stream=sys.stderr, level=logging.INFO)
+
+def quit_cb(signal_number, stack_frame):
+    logging.info("Terminate with signal %d" % signal_number)
+    mainloop.quit()
+
+def main():
+    global mainloop
+
+    parser = argparse.ArgumentParser(description="KNoT DBUS-Control Daemon")
+    parser.add_argument("-w", "--working-dir", metavar="<path>",
+                        default="/usr/local/bin",
+                        type=str,
+                        help="Daemon working directory")
+    parser.add_argument("-p", "--pid-filepath", metavar="<path/control>",
+                        default="/tmp/control", type=str,
+                        help="PID file path and name")
+    parser.add_argument("-n", "--detach-process", action="store_false",
+                        help="Detached process")
+    args = parser.parse_args()
+
+    context = daemon.DaemonContext(
+        working_directory=args.working_dir,
+        umask=0o002,
+        detach_process=args.detach_process,
+        pidfile=lockfile.FileLock(args.pid_filepath),
+        signal_map={signal.SIGTERM: quit_cb, signal.SIGINT: quit_cb},
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+    )
+
+    with context:
+        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+        mainloop = GObject.MainLoop()
+
+        mainloop.run()
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="control",
+    version="1.0",
+    author="Allysson Lukas",
+    author_email="alla@cesar.org.br",
+    description=("Daemon"),
+    license="BSD-3-Clause",
+    keywords="daemon control gateway iot dbus",
+    url="https://github.com/CESARBR/knot-gateway-control",
+    packages=find_packages(),
+    long_description=read("README.md"),
+    entry_points={
+        "console_scripts": [
+            "kcontrold = control.__main__:main"
+        ]
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD 3-Clause License",
+    ],
+)
